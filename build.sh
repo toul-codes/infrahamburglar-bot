e883d8aad8b3cca81990e06ff92eb054d1e9e1c5
Toul <toul@hey>
2022-10-19 12:13:26 -0500

    First draft that includes additional commands

---
+++ build.sh
@@ -0,0 +1,2 @@
+GOOS=linux  GOARCH=amd64 go build -o infrahamburglar main.go
+zip infrahamburglar.zip infrahamburglar
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/michimani/gotwi"
 	"github.com/michimani/gotwi/fields"
 	"github.com/michimani/gotwi/tweet/filteredstream"
@@ -15,76 +16,11 @@ import (
 	"time"
 )
 
-// ListSearchStreamRules lists search stream rules.
-func listSearchStreamRules() {
-	c, err := newGotwiClientWithTimeout(30)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	p := &types.ListRulesInput{}
-	res, err := filteredstream.ListRules(context.Background(), c, p)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	for _, r := range res.Data {
-		fmt.Printf("ID: %s, Value: %s, Tag: %s\n", gotwi.StringValue(r.ID), gotwi.StringValue(r.Value), gotwi.StringValue(r.Tag))
-	}
-}
-
-func deleteSearchStreamRules(ruleID string) {
-	c, err := newGotwiClientWithTimeout(30)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	p := &types.DeleteRulesInput{
-		Delete: &types.DeletingRules{
-			IDs: []string{
-				ruleID,
-			},
-		},
-	}
-
-	res, err := filteredstream.DeleteRules(context.TODO(), c, p)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	for _, r := range res.Data {
-		fmt.Printf("ID: %s, Value: %s, Tag: %s\n", gotwi.StringValue(r.ID), gotwi.StringValue(r.Value), gotwi.StringValue(r.Tag))
-	}
-}
-
-// createSearchStreamRules creates a search stream rule.
-func createSearchStreamRules(keyword string) {
-	c, err := newGotwiClientWithTimeout(30)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	p := &types.CreateRulesInput{
-		Add: []types.AddingRule{
-			{Value: gotwi.String(keyword), Tag: gotwi.String(keyword)},
-		},
-	}
-
-	res, err := filteredstream.CreateRules(context.TODO(), c, p)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	for _, r := range res.Data {
-		fmt.Printf("ID: %s, Value: %s, Tag: %s\n", gotwi.StringValue(r.ID), gotwi.StringValue(r.Value), gotwi.StringValue(r.Tag))
-	}
-}
+const (
+	OAuthTokenEnvKeyName       = "GOTWI_ACCESS_TOKEN"
+	OAuthTokenSecretEnvKeyName = "GOTWI_ACCESS_TOKEN_SECRET"
+	USER                       = "1510513502628331522"
+)
 
 // SimpleTweet posts a tweet with only text, and return posted tweet ID.
 func SimpleTweet(c *gotwi.Client, text string) (string, error) {
@@ -99,12 +35,6 @@ func SimpleTweet(c *gotwi.Client, text string) (string, error) {
 
 	return gotwi.StringValue(res.Data.ID), nil
 }
-
-const (
-	OAuthTokenEnvKeyName       = "GOTWI_ACCESS_TOKEN"
-	OAuthTokenSecretEnvKeyName = "GOTWI_ACCESS_TOKEN_SECRET"
-	USER                       = "1510513502628331522"
-)
 
 func newOAuth1Client() (*gotwi.Client, error) {
 	in := &gotwi.NewClientInput{
@@ -179,40 +109,5 @@ func newGotwiClientWithTimeout(timeout int) (*gotwi.Client, error) {
 }
 
 func main() {
-	args := os.Args
-
-	if len(args) < 2 {
-		fmt.Println("The 1st parameter for command is required. (create|stream)")
-		os.Exit(1)
-	}
-	command := args[1]
-	switch command {
-	case "list":
-		// list search stream rules
-		listSearchStreamRules()
-	case "delete":
-		// delete a specified rule
-		if len(args) < 3 {
-			fmt.Println("The 2nd parameter for rule ID to delete is required.")
-			os.Exit(1)
-		}
-
-		ruleID := args[2]
-		deleteSearchStreamRules(ruleID)
-	case "create":
-		// create a search stream rule
-		if len(args) < 3 {
-			fmt.Println("The 2nd parameter for keyword of search stream rule is required.")
-			os.Exit(1)
-		}
-
-		keyword := args[2]
-		createSearchStreamRules(keyword)
-	case "stream":
-		// exec filtered stream API
-		execSearchStream()
-	default:
-		fmt.Println("Undefined command. Command should be 'create' or 'stream'.")
-		os.Exit(1)
-	}
+	lambda.Start(execSearchStream)
 }
